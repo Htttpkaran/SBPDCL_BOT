391ce3f59ae2ff5c84640bba27a8179bfd0e9847
Karan <karankumar10986745@gmail.com>
2025-04-17 05:45:36 +0530

    update

--- sbpdcl_bot.py
+++ sbpdcl_bot.py
@@ -146,7 +146,6 @@ if __name__ == "__main__":
         app.run_webhook(
             listen="0.0.0.0",
             port=int(os.getenv("PORT", 8080)),
-            webhook_path="/webhook",
             webhook_url=webhook_url
         )
 
